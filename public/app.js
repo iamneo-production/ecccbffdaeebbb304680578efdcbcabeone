466d152d747ab25128e173b803e97b783857f843
Iamneo <techadmin@iamneo.ai>
2023-09-03 16:11:05 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -2,8 +2,57 @@
 let cells = ['', '', '', '', '', '', '', '', ''];
 let currentPlayer = 'X';
 let result = document.querySelector('.result');
-let restartBtn = document.querySelectorAll('#restartBtn');
-let winconditions = [
+
+// Function to handle player moves
+const ticTacToe = (cell, index) => {
+    if (cells[index] === '' && !result.textContent) {
+        cells[index] = currentPlayer;
+        cell.textContent = currentPlayer;
+        cell.classList.add(currentPlayer);
+        
+        if (checkWin(currentPlayer)) {
+            result.textContent = `${currentPlayer} wins!`;
+            disableCells();
+        } else if (cells.every(cell => cell !== '')) {
+            result.textContent = "It's a draw!";
+        } else {
+            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+            result.textContent = `Current Player: ${currentPlayer}`;
+        }
+    }
+};
+
+// Function to check for winning conditions
+const checkWin = (player) => {
+    for (const condition of winconditions) {
+        const [a, b, c] = condition;
+        if (cells[a] === player && cells[b] === player && cells[c] === player) {
+            return true;
+        }
+    }
+    return false;
+};
+
+// Function to disable all cells after a win or draw
+const disableCells = () => {
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach(cell => cell.style.pointerEvents = 'none');
+};
+
+// Function to reset the game
+const resetGame = () => {
+    cells = ['', '', '', '', '', '', '', '', ''];
+    currentPlayer = 'X';
+    result.textContent = '';
+    const cellsElements = document.querySelectorAll('.cell');
+    cellsElements.forEach(cell => {
+        cell.textContent = '';
+        cell.classList.remove('X', 'O');
+        cell.style.pointerEvents = 'auto';
+    });
+};
+
+const winconditions = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -13,52 +62,3 @@ let winconditions = [
     [0, 4, 8],
     [2, 4, 6]
 ];
-
-// Function to handle player moves
-const ticTacToe = (element, index) => {
-    // Your game logic here
-
-    /*
-    **Part 1: Winning Conditions (Add your code here)**
-
-    1. Implement the logic to check for winning conditions using the 'conditions' array.
-    2. Display a winning message in the 'result' element when a player wins.
-    3. Disable all buttons after a win.
-    */
-
-    // Your code to update the game state and check for a win
-    // ...
-
-    // Your code to display the current player's turn
-    // ...
-
-    // Your code to handle button and cell interactions
-    // ...
-};
-
-    /*
-    **Part 2: Reset Function (Add your code here)**
-
-    1. Implement a new function that resets the game to its initial state.
-    2. Ensure the 'cells', 'btns', and 'currentPlayer' variables are reset.
-    3. Update the 'result' element to indicate the current player's turn.
-    4. Re-enable all buttons for a new game.
-    */
-
-// Function to reset the game
-const resetGame = () => {
-    // Your code to reset the game state
-    // ...
-
-    // Your code to update the 'result' element
-    // ...
-
-    // Your code to re-enable buttons
-    // ...
-};
-
-btns.forEach((btn, i) => {
-    btn.addEventListener('click', () => ticTacToe(btn, i));
-});
-
-document.querySelector('#reset').addEventListener('click', resetGame);
